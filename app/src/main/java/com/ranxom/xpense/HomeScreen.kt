a93374adb784b0f266ed35fa390cb33496b5b235
Saiyed Shizain <saiyedshizain@gmail.com>
2025-01-04 17:11:36 +0530

    Init

---
+++ app/src/main/java/com/ranxom/xpense/HomeScreen.kt
@@ -0,0 +1,15 @@
+package com.ranxom.xpense
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HomeScreen() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen()
+}
